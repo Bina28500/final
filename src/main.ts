719f3c3f92ae7538e8ff4cc53cfc6aba2a3e40ad
Bina <bina2850@mail.ru>
2021-11-20 18:26:53 +0300

    final

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(join(__dirname, '../src/views'));
   app.setViewEngine('pug');
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3028);
 }
 bootstrap();
